0aff55c7c03a99e1f80924bfa2deb23d2284eebf
[GitHub1802jiwoo] <[GitHub2008jiwoo@gmail.com]>
2024-04-17 20:23:39 +0900

    commit

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:qwer2/screens/first.dart';
+import 'package:qwer2/screens/second.dart';
+import 'package:qwer2/screens/third.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My APP',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My APP'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(
+              text: 'Tab1',
+            ),
+            Tab(
+              text: 'Tab2',
+            ),
+            Tab(
+              text: 'Tab3',
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FirstScreen()),
+                );
+              },
+              child: Text(
+                'Use Stack'
+              ),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondScreen()),
+                );
+              },
+              child: Text(
+                  'Use Image'
+              ),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FirstScreen()),
+                );
+              },
+              child: Text(
+                  'Use Stack'
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
